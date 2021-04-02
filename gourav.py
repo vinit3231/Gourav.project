aafa3ef724188be6645f33090250e21dab3d0670
vinit3231 <81798527+vinit3231@users.noreply.github.com>
2021-04-02 15:22:13 +0530

    i made some changes

--- gourav.py
+++ gourav.py
@@ -1,10 +1,5 @@
-a = 0
-c = 0
-b = list()
-while a < 98:
-    a = a + 3
-    b.append(a)
-while c < 96:
-    c = c + 5
-    b.append(c)
-print(b)
+a = list()
+for i in range(1,100):
+    if(i%3==0 or i%5==0):
+        a.append(i)
+print(a)
